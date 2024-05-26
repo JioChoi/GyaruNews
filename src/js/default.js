97a51d2b790e626582ada5abd4b152ca0dfe5bbb
JioChoi <jiochoi7@gmail.com>
2024-05-26 13:04:21 +0900

    Change host

--- src/js/default.js
+++ src/js/default.js
@@ -1,4 +1,4 @@
-let host = "https://jio7-newsgaki.hf.space";
+let host = "https://jio7-gyarunews.hf.space";
 //host = "http://127.0.0.1";
 
 document.addEventListener('DOMContentLoaded', async () => {
